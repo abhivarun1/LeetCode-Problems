e1df6357c296e2e48d8a3874c8f2c812decda836
abhivarun1 <abhivarunofficial@gmail.com>
2026-08-13 11:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (42.92%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.py
+++ 2722-prime-in-diagonal/prime-in-diagonal.py
@@ -12,13 +12,9 @@ class Solution:
     def diagonalPrime(self, nums: List[List[int]]) -> int:
         res = 0
         for i in range(len(nums)):
-            maxi = 0
-            if (nums[i][i] > res) and (nums[i][i] % 2 != 0 or nums[i][i] == 2):
-                maxi = self.isPrime(nums[i][i])
-                if maxi > res:
-                    res = maxi
-            if (nums[i][len(nums) - i - 1] > res) and (nums[i][len(nums) - i - 1] % 2 != 0 or nums[i][len(nums) - i - 1] == 2):
-                maxi = self.isPrime(nums[i][len(nums) - i - 1])
-                if maxi > res:
-                    res = maxi
+            for val in (nums[i][i], nums[i][len(nums) - i - 1]):
+                if val > res and (val % 2 != 0 or val == 2):
+                    maxi = self.isPrime(val)
+                    if maxi > res:
+                        res = maxi
         return res
